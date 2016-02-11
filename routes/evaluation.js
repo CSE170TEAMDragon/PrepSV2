c6cc911ac08d7676012d1c9d6cc5b2b3def09281
Eva Huynh <e4huynh@ucsd.edu>
2016-02-11 20:58:55 +0000

    Fixed the self eval page

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -1,11 +1,6 @@
 
 exports.viewEvaluation = function(req, res) {
-	console.log("Navigating to evaluation page");
 	// var question = req.params.question;
 	// console.log(" ' " + question + " ' was the question selected");
 	res.render('evaluation');
-	// 					, {
-	// 	'questionSelected':question
-
-	// });
 }
