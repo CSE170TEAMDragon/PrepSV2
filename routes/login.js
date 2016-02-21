80688793cd31c5086a25d20bdb88b2d778262be2
Eva Huynh <e4huynh@ucsd.edu>
2016-02-21 21:33:10 +0000

    Changed the self-eval page to see better

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,10 @@
+var data = require ("../logininfo");
+
 exports.viewProject = function(req, res) { 
-  // controller code goes here 
-    res.render('login');
+	/*var name = req.query.flag;
+	if( name === "1")
+    	res.render('login');
+    else
+    	res.json(data);*/
+    res.render ('login');
 };
