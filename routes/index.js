45b2993db5bee4624ed66ce749af213a4b84d02f
Eva Huynh <e4huynh@ucsd.edu>
2016-02-25 17:28:57 +0000

    removed version B randomization

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,18 @@ var data = require ("../questions.json");
 
 exports.view = function(req, res){
   data["login"] = false;
-  var random_num = Math.random();
 
+  /*var random_num = Math.random();
   if (random_num > 0.5) {
     res.render('index');
   } 
   else {
     res.redirect('/versionB');
-  }
-
+  }*/
+  res.render ('index');
 };
 
 exports.viewVersionB = function(req, res){
+	data["login"] = false;
     res.render('indexB');
 };
