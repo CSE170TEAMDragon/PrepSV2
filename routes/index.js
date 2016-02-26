527ef248d4376a798285fcbf9da4edd261797ec6
Eva Huynh <e4huynh@ucsd.edu>
2016-02-26 05:37:52 +0000

    Submission for A8

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ var data = require ("../questions.json");
 
 exports.view = function(req, res){
   data["login"] = false;
+  res.render('index');
 
   /*var random_num = Math.random();
+
   if (random_num > 0.5) {
     res.render('index');
   } 
   else {
     res.redirect('/versionB');
   }*/
-  res.render ('index');
+
 };
 
 exports.viewVersionB = function(req, res){
